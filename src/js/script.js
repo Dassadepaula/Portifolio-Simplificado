a9f5f87976a60fb8c66f21cb0d8d7c23fb5c301f
Dassadepaula <161641347+Dassadepaula@users.noreply.github.com>
2024-04-08 12:31:23 -0300

    Add files via upload

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,8 @@
 // Objetivo 1 = quando o usuário clic no botão de mostrar mais deve abrir os projetos que estão escondidos no html
 // Passo 1 - pegar o botão mostrar mais no JS pra poder verificar quando o usuário clicar em cima dele
 const btn = document.querySelector('.btn-mostrar-projetos');
-const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)');
 btn.addEventListener('click', () => {
+    const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)');
     projetosInativos.forEach(projetoInativo => {
         projetoInativo.classList.add('ativo');
     });
